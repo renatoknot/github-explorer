6e8f03bdab0c93143dce911e54bf45289182d4bc
Renato Gonçalves <renatoslip@hotmail.com>
2022-03-19 09:30:38 -0300

    estilizando Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { FiChevronRight } from "react-icons/fi";
 
 import logoImg from "../../assets/logo.svg";
 
-import { Title, Form } from "./styles";
+import { Title, Form, Repositories } from "./styles";
 
 const Dashboard: React.FC = () => {
   return (
@@ -14,6 +15,53 @@ const Dashboard: React.FC = () => {
         <input placeholder="Digite o nome do repositório" />
         <button type="submit">Pesquisar</button>
       </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/47463666?v=4"
+            alt="Github Avatar"
+          />
+          <div>
+            <strong>renatoknot/movieland-app</strong>
+            <p>
+              Movieland is a mini project to search for movies, using React.js I
+              learned to work with the api of the OMDb website (it's free)
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/47463666?v=4"
+            alt="Github Avatar"
+          />
+          <div>
+            <strong>renatoknot/movieland-app</strong>
+            <p>
+              Movieland is a mini project to search for movies, using React.js I
+              learned to work with the api of the OMDb website (it's free)
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/47463666?v=4"
+            alt="Github Avatar"
+          />
+          <div>
+            <strong>renatoknot/movieland-app</strong>
+            <p>
+              Movieland is a mini project to search for movies, using React.js I
+              learned to work with the api of the OMDb website (it's free)
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   );
 };
