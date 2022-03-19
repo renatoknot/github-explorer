a7f2c42f8deab7c1f7017d7f7f639f3fa72421de
Renato Gonçalves <renatoslip@hotmail.com>
2022-03-19 11:17:08 -0300

    conectando à api do github

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,66 +1,68 @@
-import React from "react";
+import React, { useState, FormEvent } from "react";
 import { FiChevronRight } from "react-icons/fi";
+import api from "../../services/api";
 
 import logoImg from "../../assets/logo.svg";
 
 import { Title, Form, Repositories } from "./styles";
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState("");
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>
+  ): Promise<void> {
+    // Adição de um novo Repositório
+    // Consumir a api do Github
+    // Salvar o novo respositório no estado
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo("");
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          placeholder="Digite o nome do repositório"
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars.githubusercontent.com/u/47463666?v=4"
-            alt="Github Avatar"
-          />
-          <div>
-            <strong>renatoknot/movieland-app</strong>
-            <p>
-              Movieland is a mini project to search for movies, using React.js I
-              learned to work with the api of the OMDb website (it's free)
-            </p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img
-            src="https://avatars.githubusercontent.com/u/47463666?v=4"
-            alt="Github Avatar"
-          />
-          <div>
-            <strong>renatoknot/movieland-app</strong>
-            <p>
-              Movieland is a mini project to search for movies, using React.js I
-              learned to work with the api of the OMDb website (it's free)
-            </p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img
-            src="https://avatars.githubusercontent.com/u/47463666?v=4"
-            alt="Github Avatar"
-          />
-          <div>
-            <strong>renatoknot/movieland-app</strong>
-            <p>
-              Movieland is a mini project to search for movies, using React.js I
-              learned to work with the api of the OMDb website (it's free)
-            </p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repositories.map((repository) => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
